4e101716be46036d06a2eea992e8d405c8626524
splendor-kill <splendor.kill@gmail.com>
2021-09-25 01:20:48 +0800

    repr board in terminal

--- src/ChessBoard.py
+++ src/ChessBoard.py
@@ -1,3 +1,5 @@
+from enum import IntEnum
+
 from chessman.Bing import *
 from chessman.Che import *
 from chessman.Ma import *
@@ -6,10 +8,49 @@ from chessman.Shi import *
 from chessman.Shuai import *
 from chessman.Xiang import *
 
-empty_board = '''
+APPEAR_BLACK = '\033[1;30;47m'
+APPEAR_RED = '\033[1;31;47m'
+ENDC = '\033[0m'
+
+
+class Camp(IntEnum):
+    BLACK = 1
+    RED = 2
+
+
+class Force(IntEnum):
+    SHUAI = 1
+    SHI = 2
+    XIANG = 3
+    MA = 4
+    JU = 5
+    PAO = 6
+    BING = 7
+
+
+PIECE_CHARS = '將士象馬車砲卒帥仕相傌俥炮兵'
+PLACE_CHARS = '＋Ｘ'
+
+
+def piece_2_char(camp, force):
+    index = 7 * (camp.value - 1) + force.value - 1
+    return PIECE_CHARS[index]
+
+
+def recog_piece(piece: str):
+    assert piece in PIECE_CHARS
+    assert len(piece) == 1
+    idx = PIECE_CHARS.index(piece)
+    q, r = divmod(idx, len(Force))
+    camp = Camp(q + 1)
+    force = Force(r + 1)
+    return camp, force
+
+
+FULL_BOARD = '''
 車－馬－象－士－將－士－象－馬－車
 ｜　｜　｜　｜＼｜／｜　｜　｜　｜
-＋－＋－＋－＋－Ｘ－＋－＋－＋－＋
+＋－＋－＋－＋－＋－＋－＋－＋－＋
 ｜　｜　｜　｜／｜＼｜　｜　｜　｜
 ＋－砲－＋－＋－＋－＋－＋－砲－＋
 ｜　｜　｜　｜　｜　｜　｜　｜　｜
@@ -22,9 +63,30 @@ empty_board = '''
 ｜　｜　｜　｜　｜　｜　｜　｜　｜
 ＋－炮－＋－＋－＋－＋－＋－炮－＋
 ｜　｜　｜　｜＼｜／｜　｜　｜　｜
-＋－＋－＋－＋－Ｘ－＋－＋－＋－＋
+＋－＋－＋－＋－＋－＋－＋－＋－＋
 ｜　｜　｜　｜／｜＼｜　｜　｜　｜
 俥－傌－相－仕－帥－仕－相－傌－俥
+'''
+
+EMPTY_BOARD = '''
+{p00}－{p01}－{p02}－{p03}－{p04}－{p05}－{p06}－{p07}－{p08}
+｜　｜　｜　｜＼｜／｜　｜　｜　｜
+{p10}－{p11}－{p12}－{p13}－{p14}－{p15}－{p16}－{p17}－{p18}
+｜　｜　｜　｜／｜＼｜　｜　｜　｜
+{p20}－{p21}－{p22}－{p23}－{p24}－{p25}－{p26}－{p27}－{p28}
+｜　｜　｜　｜　｜　｜　｜　｜　｜
+{p30}－{p31}－{p32}－{p33}－{p34}－{p35}－{p36}－{p37}－{p38}
+｜　｜　｜　｜　｜　｜　｜　｜　｜
+{p40}－{p41}－{p42}－{p43}－{p44}－{p45}－{p46}－{p47}－{p48}
+{p50}－{p51}－{p52}－{p53}－{p54}－{p55}－{p56}－{p57}－{p58}
+｜　｜　｜　｜　｜　｜　｜　｜　｜
+{p60}－{p61}－{p62}－{p63}－{p64}－{p65}－{p66}－{p67}－{p68}
+｜　｜　｜　｜　｜　｜　｜　｜　｜
+{p70}－{p71}－{p72}－{p73}－{p74}－{p75}－{p76}－{p77}－{p78}
+｜　｜　｜　｜＼｜／｜　｜　｜　｜
+{p80}－{p81}－{p82}－{p83}－{p84}－{p85}－{p86}－{p87}－{p88}
+｜　｜　｜　｜／｜＼｜　｜　｜　｜
+{p90}－{p91}－{p92}－{p93}－{p94}－{p95}－{p96}－{p97}－{p98}
 '''
 
 
@@ -135,15 +197,87 @@ class ChessBoard:
 
 
 class Board:
-    def __init__(self):
-        self.situation = {}
+    def __init__(self, situation):
+        if isinstance(situation, str):
+            situation = self._parse(situation)
+        self.situation = situation
 
     def __str__(self):
-        pass
+        sit = {f'p{i}{j}': PLACE_CHARS[0] for i in range(10) for j in range(9)}
+        for p in self.situation:
+            k = f'p{p.x}{p.y}'
+            sit[k] = str(p)
+        return EMPTY_BOARD.format(**sit)
 
-    def _formation(self):
-        pass
+    @staticmethod
+    def _parse(board):
+        board = board.strip()
+        rows = board.splitlines()
+        rows = rows[:9:2] + rows[-9::2]
+        assert len(rows) == 10
+        sit = []
+        for i, r in enumerate(rows):
+            r = r.strip()
+            assert len(r) == 17
+            r = r[::2]  # remove non-place chars
+            for j, c in enumerate(r):
+                if c in PIECE_CHARS:
+                    camp, force = recog_piece(c)
+                    sit.append(Piece(i, j, camp, force))
+        return sit
+
+
+class Piece:
+    def __init__(self, x, y, camp, force):
+        self.x = x
+        self.y = y
+        self.camp = camp
+        self.force = force
+
+    def __str__(self):
+        c = piece_2_char(self.camp, self.force)
+        appear = APPEAR_RED if self.camp == Camp.RED else APPEAR_BLACK
+        c = appear + c + ENDC
+        return c
+
+
+def test():
+    situation = [Piece(0, 4, Camp.BLACK, Force.SHUAI),
+                 Piece(2, 3, Camp.BLACK, Force.SHI), Piece(2, 4, Camp.RED, Force.BING),
+                 Piece(2, 8, Camp.BLACK, Force.XIANG),
+                 Piece(3, 0, Camp.BLACK, Force.BING),
+                 Piece(5, 6, Camp.BLACK, Force.MA),
+                 Piece(6, 2, Camp.RED, Force.BING), Piece(6, 6, Camp.BLACK, Force.BING),
+                 Piece(7, 4, Camp.RED, Force.JU),
+                 Piece(8, 3, Camp.BLACK, Force.BING), Piece(8, 5, Camp.BLACK, Force.BING),
+                 Piece(9, 4, Camp.RED, Force.SHUAI),
+                 ]
+    board = Board(situation)
+    print(board)
+
+    situation = '''
+    ＋－＋－象－士－將－＋－＋－＋－＋
+    ｜　｜　｜　｜＼｜／｜　｜　｜　｜
+    ＋－＋－＋－＋－士－＋－＋－＋－＋
+    ｜　｜　｜　｜／｜＼｜　｜　｜　｜
+    ＋－＋－＋－＋－象－＋－＋－＋－＋
+    ｜　｜　｜　｜　｜　｜　｜　｜　｜
+    ＋－＋－＋－＋－＋－＋－＋－＋－＋
+    ｜　｜　｜　｜　｜　｜　｜　｜　｜
+    ＋－＋－＋－＋－＋－＋－傌－俥－俥
+    ＋－＋－相－＋－＋－＋－＋－＋－＋
+    ｜　｜　｜　｜　｜　｜　｜　｜　｜
+    ＋－車－兵－＋－兵－＋－＋－＋－＋
+    ｜　｜　｜　｜　｜　｜　｜　｜　｜
+    ＋－＋－＋－卒－相－＋－＋－＋－＋
+    ｜　｜　｜　｜＼｜／｜　｜　｜　｜
+    ＋－＋－＋－＋－卒－＋－＋－＋－＋
+    ｜　｜　｜　｜／｜＼｜　｜　｜　｜
+    ＋－＋－＋－帥－＋－＋－＋－＋－＋
+    '''
+    board = Board(situation)
+    print(board)
 
 
 if __name__ == '__main__':
-    print(empty_board)
+    test()
