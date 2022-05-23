839577dfc69023f323f0fbb2e4d72a1b795da330
Splendor <splendor.kill@gmail.com>
2022-05-23 12:31:30 +0800

    FEN white -> red

--- src/web/static/mcts.js
+++ src/web/static/mcts.js
@@ -10,21 +10,22 @@ function MCTS(pos, hashLevel) {
 MCTS.prototype.searchMain = function (depth, millis, cb) {
     // console.log(this.pos.toFen())
 
-    var http = new XMLHttpRequest();
-    var url = "http://127.0.0.1:5000/play";
+    const http = new XMLHttpRequest();
+    let url = "http://127.0.0.1:5000/play";
     http.open("POST", url, true);
     http.setRequestHeader("Content-Type", "application/json");
 
     http.onreadystatechange = function () {
         if (http.readyState == 4 && http.status == 200) {
             // console.log(http.responseText);
-            var s = JSON.parse(http.responseText)["move"];
-            var t = [s.slice(0, 2), "-", s.slice(2)].join("").toUpperCase();
-            var mv = iccs2Move(t);
-            cb(mv);
+            let s = JSON.parse(http.responseText)["move"];
+            let t = [s.slice(0, 2), "-", s.slice(2)].join("").toUpperCase();
+            cb(iccs2Move(t));
         }
     }
+    let fen = this.pos.toFen();
+    fen = fen.replace(" w", " r")  // white replace by red
     http.send(JSON.stringify({
-        "position": this.pos.toFen()
+        "position": fen
     }));
 }
